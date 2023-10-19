e49189647958f7231a1a64bc86b1fa0393d1c6d5
pypymi <dr.mizoguchi@gmail.com>
2023-10-20 07:55:38 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const filterButtons = document.querySelectorAll(".filter_buttons button")
+const filterrableCards = document.querySelectorAll(".filterable_cards .card")
+
+const filterCards = e => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active")
+
+  filterrableCards.forEach(card => {
+    card.classList.add("hide")
+    if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+      card.classList.remove("hide")
+    }
+  })
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
